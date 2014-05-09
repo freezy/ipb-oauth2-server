fdf88216b3db47a448a8c00f58daa511a862ef88
freezy <freezy@xbmc.org>
2014-05-09 13:31:32 +0200

    Added OAuth2 Server PHP library.

--- modules_public/server/authorize.php
+++ modules_public/server/authorize.php
@@ -1,7 +1,12 @@
 <?php
 
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'sources' . DIRECTORY_SEPARATOR . 'IPB' . DIRECTORY_SEPARATOR . 'Storage.php';
+
 class public_oauth2_server_authorize extends ipsCommand {
     public function doExecute(ipsRegistry $registry) {
+
+		$storage = new \IPB\Storage($this->DB);
         print "This would be the authorization page.";
+		$storage->getAccessToken('asdf');
     }
 }
