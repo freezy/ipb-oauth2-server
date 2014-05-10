9558a0f3b9e8fad18580ad25d77533b4862e23ec
freezy <freezy@xbmc.org>
2014-05-10 15:57:27 +0200

    Added admin control panel for auth client creation (WIP).

--- modules_admin/manager/clients.php
+++ modules_admin/manager/clients.php
@@ -15,12 +15,21 @@ class admin_oauth2_manager_clients extends ipsCommand {
 
 	public function doExecute(ipsRegistry $registry) {
 
-		//$this->registry->getClass('class_localization')->loadLanguageFile(array('admin_global'), 'oauth2');
 		$this->lang->loadLanguageFile(array('admin_lang'), 'oauth2');
 		$this->html = $this->registry->output->loadTemplate('cp_skin_oauth2');
 
 		$this->form_code = $this->html->form_code = 'module=manager&amp;section=clients';
 		$this->form_code_js = $this->html->form_code_js = 'module=manager&section=clients';
+
+		switch ($this->request['do']) {
+			case 'create':
+				$html = $this->createClient();
+				break;
+			case 'list':
+			default:
+				$html = $this->html->listClients(array());
+				break;
+		}
 
 		/*
 		 * seriously, this is pathetic. doc[1][2] clearly says to use output::addContent(),
@@ -29,9 +38,43 @@ class admin_oauth2_manager_clients extends ipsCommand {
 		 * [1] http://www.invisionpower.com/support/guides/_/advanced-and-developers/application/using-skin-templates-r154
 		 * [2] http://www.invisionpower.com/support/guides/_/advanced-and-developers/api-methods/outputting-html-r194
 		 */
-		$this->registry->output->html .= $this->html->listClients(array());
-
+		$this->registry->output->html .= $html;
 		$this->registry->output->html_main .= $this->registry->getClass('output')->global_template->global_frame_wrapper();
 		$this->registry->output->sendOutput();
+	}
+
+	private function createClient() {
+		return $this->form('add');
+	}
+
+	private function form($type = 'add') {
+
+		$client_id = $this->request['client_id'];
+		$form = array();
+		$permissions = array();
+
+		if ($type == 'add') {
+			$lang = array(
+				'title' => $this->lang->words['o_new_client'],
+				'subtitle' => $this->lang->words['o_new_client_short'],
+				'button' => $this->lang->words['o_create'],
+				'info' => $this->lang->words['o_create_info'],
+			);
+			$formcode = 'add_save';
+			$client = array();
+		}
+
+		$form['client_name'] = $this->registry->output->formInput('client_name', !empty($_POST['client_name']) ? stripslashes($_POST['client_name']) : $client['client_name']);
+		$form['client_url'] = $this->registry->output->formInput('client_url', !empty($_POST['client_url']) ? stripslashes($_POST['client_url']) : $client['client_url']);
+
+		if ($type == 'add') {
+			$form['_client_id'] = substr(md5(mt_rand() . $this->memberData['member_login_key'] . uniqid(mt_rand(), true)), 0, 20);
+			$form['_client_secret'] = substr(md5(mt_rand() . $this->memberData['member_login_key'] . uniqid(mt_rand(), true)) . md5(uniqid(mt_rand(), true)), 0, 40);
+		}
+
+		$html = $this->registry->output->global_template->information_box($this->lang->words['a_title'], $this->lang->words['a_msg2']) . "<br />";
+		$html .= $this->html->form($form, $lang, $formcode, $client, $type);
+
+		return $html;
 	}
 }
--- skin_cp/cp_skin_oauth2.php
+++ skin_cp/cp_skin_oauth2.php
@@ -16,7 +16,7 @@ $IPBHTML .= <<<EOF
 	<div class='ipsActionBar clearfix'>
 		<ul>
 			<li class='ipsActionButton'>
-				<a href='{$this->settings['base_url']}&amp;{$this->form_code}&amp;do=api_add'><img src='{$this->settings['skin_acp_url']}/images/icons/add.png' alt='' /> Add Client</a>
+				<a href='{$this->settings['base_url']}&amp;{$this->form_code}&amp;do=create'><img src='{$this->settings['skin_acp_url']}/images/icons/add.png' alt='' /> Add Client</a>
 			</li>
 		</ul>
 	</div>
@@ -61,7 +61,7 @@ EOF;
 $IPBHTML .= <<<EOF
 		<tr>
 			<td colspan='5' class='no_messages'>
-				{$this->lang->words['o_no_clients']} <a href='{$this->settings['base_url']}&amp;{$this->form_code}&amp;do=api_add' class='mini_button'>{$this->lang->words['o_new_client']}</a>
+				{$this->lang->words['o_no_clients']} <a href='{$this->settings['base_url']}&amp;{$this->form_code}&amp;do=create' class='mini_button'>{$this->lang->words['o_new_client']}</a>
 			</td>
 		 </tr>
 EOF;
@@ -75,4 +75,89 @@ EOF;
 //--endhtml--//
 return $IPBHTML;
 }
+
+function form( $form, $lang, $formcode, $client, $type) {
+
+$IPBHTML = "";
+//--starthtml--//
+
+// header
+$IPBHTML .= <<<EOF
+<div class='section_title'>
+	<h2>{$lang['title']}</h2>
+	<div class='clearfix'></div>
+</div>
+<div class="section_info">
+	You may create API users for use with the XML-RPC system which allows other applications to access IP.Board data
+</div>
+
+<form id='mainform' action='{$this->settings['base_url']}{$this->form_code}&amp;do={$formcode}&amp;client={$api_user['client_id']}' method='post'>
+	<div class='acp-box'>
+ 		<h3>{$lang['subtitle']}</h3>
+
+ 		<table class='ipsTable double_pad'>
+			<tr>
+				<th colspan='2'>{$this->lang->words['a_userbasics']}</th>
+			</tr>
+EOF;
+
+		if ($type == 'add') {
+$IPBHTML .= <<<EOF
+			<tr>
+				<td class='field_title'>
+					<strong class='title'>{$this->lang->words['o_client_id']}</strong>
+				</td>
+				<td class='field_field'>
+					<input type='hidden' name='api_user_key' value='{$form['_client_id']}' />
+					<strong>{$form['_client_id']}</strong><br />
+				</td>
+			</tr>
+			<tr>
+				<td class='field_title'>
+					<strong class='title'>{$this->lang->words['o_client_secret']}</strong>
+				</td>
+				<td class='field_field'>
+					<input type='hidden' name='api_user_key' value='{$form['_client_secret']}' />
+					<strong>{$form['_client_secret']}</strong><br />
+				</td>
+			</tr>
+EOF;
+		}
+
+$IPBHTML .= <<<EOF
+			<tr>
+				<td class='field_title'>
+					<strong class='title'>{$this->lang->words['a_usertitle']}</strong>
+				</td>
+				<td class='field_field'>
+					{$form['api_user_name']}<br />
+					<span class='desctext'>{$this->lang->words['a_usertitle_info']}</span>
+				</td>
+			</tr>
+			<tr>
+				<td class='field_title'>
+					<strong class='title'>{$this->lang->words['a_restrictip']}</strong>
+				</td>
+				<td class='field_field'>
+					{$form['api_user_ip']}<br />
+					<span class='desctext'>{$this->lang->words['a_restrictip_info']}</span>
+				</td>
+			</tr>
+			<tr>
+				<th colspan='2'>{$this->lang->words['a_grant_functions']}</th>
+			</tr>
+		</table>
+EOF;
+
+
+$IPBHTML .= <<<EOF
+			</ul>
+		</div>
+EOF;
+
+
+//--endhtml--//
+return $IPBHTML;
+}
+
 }
